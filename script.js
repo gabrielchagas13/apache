27014fd5bdd431707810489d7d0f811a9792147d
Gabriel Chagas Lage <107369547+gabrielchagas13@users.noreply.github.com>
2023-10-24 23:50:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-const counters = document.querySelectorAll('.counter')
+let [computador_pontuacao, usuario_pontuacao] = [0, 0];
+let resultado_ref = document.getElementById("resultado");
+let escolhas_objeto = {
+  pedra: {
+    pedra: "empate",
+    tesoura: "vitoria",
+    papel: "derrota",
+  },
+  tesoura: {
+    pedra: "derrota",
+    tesoura: "empate",
+    papel: "vitoria",
+  },
+  papel: {
+    pedra: "vitoria",
+    tesoura: "derrota",
+    papel: "empate",
+  },
+};
 
-counters.forEach(counter => {
-    counter.innerText = '0'
+function verificar(entrada) {
+  var escolhas = ["pedra", "tesoura", "papel"];
+  var numero = Math.floor(Math.random() * 3);
 
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-target')
-        const c = +counter.innerText
+  document.getElementById(
+    "computador_escolha"
+  ).innerHTML = `Computador escolheu <span> ${escolhas[
+    numero
+  ].toUpperCase()} </span>`;
+  document.getElementById(
+    "usuario_escolha"
+  ).innerHTML = `Você escolheu <span> ${entrada.toUpperCase()} </span>`;
 
-        const increment = target / 200
+  let computador_escolha = escolhas[numero];
 
-        if(c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`
-            setTimeout(updateCounter, 1)
-        } else {
-            counter.innerText = target
-        }
-    }
+  switch (escolhas_objeto[entrada][computador_escolha]) {
+    case "vitoria":
+      resultado_ref.style.cssText = "background-color: #cefdce; color: #689f38";
+      resultado_ref.innerHTML = "VOCÊ VENCEU";
+      usuario_pontuacao++;
+      break;
+    case "derrota":
+      resultado_ref.style.cssText = "background-color: #ffdde0; color: #d32f2f"; 
+      resultado_ref.innerHTML = "VOCÊ PERDEU";
+      computador_pontuacao++;
+      break;
+    default:
+      resultado_ref.style.cssText = "background-color: #e5e5e5; color: #808080";
+      resultado_ref.innerHTML = "EMPATOU";
+      break;
+  }
 
-    updateCounter()
-})
+  document.getElementById("computador_pontuacao").innerHTML =
+    computador_pontuacao;
+  document.getElementById("usuario_pontuacao").innerHTML = usuario_pontuacao;
+}
